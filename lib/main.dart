483d16ebeb82a274145f2e5baac966e2c2f32f01
coolbuhzor <coolbuhzor@gmail.com>
2023-05-24 17:57:12 +0100

    dice app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,11 +16,80 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+
+  void rollDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [],
+    // leftDiceNumber = 2;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Row(
+            children: [
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {},
+                  child: Image.asset(
+                    'images/dice$leftDiceNumber.png',
+                  ),
+                ),
+              ),
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {},
+                  child: Image.asset(
+                    'images/dice$rightDiceNumber.png',
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 100.0,
+        ),
+        Container(
+          width: 200,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.white,
+              width: 2.0,
+            ),
+            borderRadius: BorderRadius.circular(8),
+          ), // Set the desired width of the button
+          child: MaterialButton(
+            onPressed: () {
+              rollDice();
+            },
+            child: Text(
+              'Roll Dice',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            textColor: Colors.white,
+            padding: EdgeInsets.all(16),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
